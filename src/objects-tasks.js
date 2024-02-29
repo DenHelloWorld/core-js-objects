4c1a3299f6da226e4ddc3662a1fe76f3c44a4b70
Denis Saveliev <denissaveliev23@gmail.com>
2024-02-29 22:36:19 +0200

    feat: solve 3 tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -117,8 +117,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze({ ...obj });
 }
 
 /**
@@ -131,8 +131,16 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+
+function makeWord(lettersObject) {
+  const wordArray = [];
+  const keys = Object.keys(lettersObject);
+  keys.forEach((letter) => {
+    lettersObject[letter].forEach((position) => {
+      wordArray[position] = letter;
+    });
+  });
+  return wordArray.join('');
 }
 
 /**
@@ -149,8 +157,28 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let twentyFives = 0;
+  let fifties = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      twentyFives += 1;
+    } else if (queue[i] === 50) {
+      if (twentyFives === 0) return false;
+      twentyFives -= 1;
+      fifties += 1;
+    } else if (queue[i] === 100) {
+      if (fifties > 0 && twentyFives > 0) {
+        fifties -= 1;
+        twentyFives -= 1;
+      } else if (twentyFives >= 3) {
+        twentyFives -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
